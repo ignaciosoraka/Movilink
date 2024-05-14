0658d45c8723a6163cbc9187b7aaabffa8cf8403
ignaciosoraka <ignaciosoraka@gmail.com>
2024-05-13 23:10:00 -0300

    arreglos

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const nightContainer = document.getElementById('night');
 
 // Función para generar un retraso aleatorio en milisegundos
 function getRandomDelay() {
-  return Math.random() * 5000; // Puedes ajustar el rango del retraso según tus preferencias
+  return Math.random() * 1000; // Puedes ajustar el rango del retraso según tus preferencias
 }
 
+// Función para verificar si el dispositivo es móvil
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Determina la cantidad de estrellas a generar según el tipo de dispositivo
+const starCount = isMobileDevice() ? 50 : 50;
+
 // Crea las estrellas de manera dinámica y las agrega al contenedor
-for (let i = 0; i < 100; i++) { // Genera 100 estrellas, ajusta según sea necesario
+for (let i = 0; i < starCount; i++) {
   const star = document.createElement('div');
   star.classList.add('shooting_star');
   
   // Posición aleatoria para cada estrella
-  const randomTop = Math.floor(Math.random() * window.innerHeight); // Posición aleatoria vertical
-  const randomLeft = Math.floor(Math.random() * window.innerWidth); // Posición aleatoria horizontal
+  const randomTop = Math.floor(Math.random() * window.innerHeight);
+  const randomLeft = Math.floor(Math.random() * window.innerWidth);
   star.style.top = randomTop + 'px';
   star.style.left = randomLeft + 'px';
   
